dbfd4f2f2de6bd160d58f1d4a06bdfe17342a4c1
Wirayuda Senjaya <wirayuda.senjaya@dexagroup.com>
2024-05-07 09:48:16 +0700

    create dockerfile for notifications service

--- notifications/src/index.ts
+++ notifications/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8003
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 5003
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
