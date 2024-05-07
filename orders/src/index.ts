3202dda525687620918cecb83a4eda4aa9354b0f
Wirayuda Senjaya <wirayuda.senjaya@dexagroup.com>
2024-05-07 09:45:02 +0700

    create dockerfile for orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8002
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 5002
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
